fe7f03b88f0450c27c00c99437d64eddc9328686
Josue G. Tavara Idrogo <joshuatavara23@gmail.com>
2020-08-03 17:21:11 -0500

    add dark mode - part 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
@@ -21,19 +21,40 @@ const initialState = {
 const store = createStore(reducer, initialState); // 3 params by default: reducer, initialState, enhacer
 
 function App() {
+	const [ darkMode, setDarkMode ] = useState(false);
+	const [ checked, setChecked ] = useState(false);
+	const mainClass = darkMode ? 'is-dark-mode' : 'is-light-mode';
+
+	function changeMedia(mq) {
+		setDarkMode(mq.matches);
+		setChecked(mq.matches);
+	}
+
+	useEffect(() => {
+		const mq = window.matchMedia('(prefers-color-scheme: dark)');
+		mq.addListener(changeMedia);
+		setDarkMode(mq.matches);
+		setChecked(mq.matches);
+		return () => {
+			mq.removeListener(changeMedia);
+		};
+	}, []);
+
 	return (
-		<Provider store={store}>
-			<Router>
-				<Header />
-				<Switch>
-					<Route path="/country/:id" component={CountryPage} />
-					<Route path="/">
-						<ActionList />
-						<CountryList />
-					</Route>
-				</Switch>
-			</Router>
-		</Provider>
+		<main class={mainClass}>
+			<Provider store={store}>
+				<Router>
+					<Header />
+					<Switch>
+						<Route path="/country/:id" component={CountryPage} />
+						<Route path="/">
+							<ActionList />
+							<CountryList />
+						</Route>
+					</Switch>
+				</Router>
+			</Provider>
+		</main>
 	);
 }
 
