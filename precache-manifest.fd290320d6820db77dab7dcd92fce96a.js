df72998981b8ad25b76ed88b6fade69ca35a3216
Josue G. Tavara Idrogo <joshuatavara23@gmail.com>
2020-08-03 20:43:32 -0500

    Updates

---
+++ precache-manifest.fd290320d6820db77dab7dcd92fce96a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a880a2d265833578bca802610cd049",
+    "url": "/country-flags/index.html"
+  },
+  {
+    "revision": "fc4a1f0b282353658b80",
+    "url": "/country-flags/static/css/main.d5d25730.chunk.css"
+  },
+  {
+    "revision": "b2cbb992e33f0122f671",
+    "url": "/country-flags/static/js/2.99a08098.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/country-flags/static/js/2.99a08098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4a1f0b282353658b80",
+    "url": "/country-flags/static/js/main.5a3319f9.chunk.js"
+  },
+  {
+    "revision": "c395655830e580e74f18",
+    "url": "/country-flags/static/js/runtime-main.e19465f3.js"
+  }
+]);
